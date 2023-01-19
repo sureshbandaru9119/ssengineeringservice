b138984e724f945d65ac3592534a9799a6300837
sureshbandaru9119 <115683972+sureshbandaru9119@users.noreply.github.com>
2023-01-19 10:19:58 +0530

    scroll 1

--- main.js
+++ main.js
@@ -24,13 +24,13 @@ $(document).ready(function(){
 
       var hash = this.hash;
 
-      $('html, body').animate({
+      $('html', 'body').animate({
         scrollTop: $(hash).offset().top
       }, 1000, function(){
    
         window.location.hash = hash;
       });
-    }
+    };
   });
 });
 
